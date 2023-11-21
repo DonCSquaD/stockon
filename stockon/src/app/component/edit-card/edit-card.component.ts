4d0d634d344f245e87a0bb27c0bcb0fcc5337344
victorkashima <victorkashima04@gmail.com>
2023-11-21 17:18:38 -0300

    FIX: CRUD working

--- stockon/src/app/component/edit-card/edit-card.component.ts
+++ stockon/src/app/component/edit-card/edit-card.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 import { CardService } from 'src/app/service/card.service';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, NgForm  } from '@angular/forms';
 import { Location } from '@angular/common';
 
 @Component({
@@ -9,11 +9,23 @@ import { Location } from '@angular/common';
   templateUrl: './edit-card.component.html',
   styleUrls: ['./edit-card.component.css']
 })
+
 export class EditCardComponent {
 
   currentUrl: string
 
-  data: any = []
+  editCard = new FormGroup({
+    name: new FormControl(''),
+    brand: new FormControl(''),
+    weight: new FormControl(''),
+    model: new FormControl(''),
+    thumb: new FormControl(''),
+    color: new FormControl(''),
+    validitydate: new FormControl(''),
+    quantity: new FormControl(''),
+    purchasedate: new FormControl(''),
+    description: new FormControl('')
+  })
   
   constructor(
     private cardService: CardService,
@@ -29,25 +41,32 @@ export class EditCardComponent {
   }
 
   ngOnInit(): void {
-    this.currentUrl = this.onlyNumber(this.currentUrl)
-    this.data = this.cardService.getCard(this.currentUrl)
+    this.currentUrl = this.onlyNumber(this.currentUrl),
+    console.log(this.currentUrl)
+    this.cardService.getCard(this.currentUrl).subscribe((result) => {
+      this.editCard = new FormGroup({
+        name: new FormControl(result['name']),
+        brand: new FormControl(result['brand']),
+        weight: new FormControl(result['weight']),
+        model: new FormControl(result['model']),
+        thumb: new FormControl(result['thumb']),
+        color: new FormControl(result['color']),
+        validitydate: new FormControl(result['validitydate']),
+        quantity: new FormControl(result['quantity']),
+        purchasedate: new FormControl(result['purchasedate']),
+        description: new FormControl(result['description']),
+      })
+    })
+  }
+
+  updateCard() {
+    this.cardService.editCard(this.currentUrl, this.editCard.value).subscribe((result) => {
+      alert(result + "Produto atualizado com sucesso!")
+    })
   }
 
   onlyNumber(str: string): string {
     return str.replace(/\D/g, '');
-  }
-
-  cardFormInput = {
-    name: '',
-    brand: '',
-    weight: '',
-    model: '',
-    thumb: '',
-    color: '',
-    validitydate: '',
-    quantity: '',
-    purchasedate: '',
-    description: '',
   }
  
   postCard(card: NgForm) {
